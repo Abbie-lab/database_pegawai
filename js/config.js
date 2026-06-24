ba261bab49a8c0b5260d1b88eff4e7c474edae62
HENDRIYAN HASBY PRATAMA <hendriyanhasby1997@gmail.com>
2026-06-24 08:53:56 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = "https://amvqyeiypfzgjwriuuwl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtdnF5ZWl5cGZ6Z2p3cml1dXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwOTQ0MTcsImV4cCI6MjA5NzY3MDQxN30.Ndq52QI8hOrbttGMk1bBGFHGmLmXhB6v5iddfc0_-oM";
 
 // DIUBAH: Menggunakan nama variabel 'supabaseClient' agar tidak bentrok dengan objek 'supabase' bawaan CDN
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
